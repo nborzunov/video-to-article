13a2f0cc37483ede71c418c4cc2a8bfcef985860
Nikolay Borzunov <forest3694@ya.ru>
2023-06-24 18:35:51 +0400

    async task setup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from faster_whisper.transcribe import Segment
 from pydub import AudioSegment
 import time
-from fastapi import FastAPI
+from fastapi import BackgroundTasks, FastAPI
 import openai
 import whisper
 import uuid
@@ -9,6 +9,9 @@ from pytube import YouTube
 import jax
 from faster_whisper import WhisperModel
 from fastapi.middleware.cors import CORSMiddleware
+import cv2
+import time
+import asyncio
 
 jax.config.update('jax_platform_name', 'cpu')
 
@@ -55,15 +58,13 @@ def transcribe(file_name):
                 prev_sentence = f'{prev_sentence} {text}'
 
                 if text[-1] in ["!", ".", "?"]:
-                    result.append({"start": start_timestamp, "end": word.end, "text": prev_sentence.strip() })
+                    result.append({"start": start_timestamp, "end": word.end, "text": prev_sentence.strip()})
                     prev_sentence = ''
                 if len(result) >= 1:
                     print(result[-1])
 
     end = time.time()
     print('[Transcription] %ss' % round(end - start, 2))
-
-
 
     return result
 
@@ -102,6 +103,79 @@ def get_post(text, segments):
     return response
 
 
+def is_frame_static(frame, previous_frame, static_threshold=3):
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_previous_frame = cv2.cvtColor(previous_frame, cv2.COLOR_BGR2GRAY)
+    difference = cv2.absdiff(gray_frame, gray_previous_frame)
+    non_zero_pixels = cv2.countNonZero(difference)
+    return non_zero_pixels <= static_threshold
+
+
+def is_duplicate_frame(frame, frames, similarity_threshold=0.95):
+    for existing_frame in frames:
+        similarity = compare_frames(frame, existing_frame)
+        if similarity >= similarity_threshold:
+            return True
+    return False
+
+
+def compare_frames(frame1, frame2):
+    gray_frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    gray_frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+    similarity = cv2.matchTemplate(gray_frame1, gray_frame2, cv2.TM_CCOEFF_NORMED)
+    return similarity[0][0]
+
+
+def extract_static_frames(video_path, output_path, static_duration=5, skip_frames=20):
+    capture = cv2.VideoCapture(video_path)
+    success, frame = capture.read()
+    previous_frame = frame
+    frame_count = 0
+    static_frames = []
+    static_start_frame = None
+    frame_info = []
+
+    start_time = time.time()
+
+    while success:
+        frame_count += 1
+        if frame_count % skip_frames == 0:
+            if is_frame_static(frame, previous_frame):
+                if static_start_frame is None:
+                    static_start_frame = frame_count
+                elif frame_count - static_start_frame >= static_duration:
+                    if not is_duplicate_frame(previous_frame, static_frames):
+                        static_frames.append(previous_frame)
+                        frame_info.append((len(static_frames), frame_count / capture.get(cv2.CAP_PROP_FPS)))
+                    static_start_frame = None
+            else:
+                static_start_frame = None
+
+        previous_frame = frame
+        success, frame = capture.read()
+
+    capture.release()
+
+    for i, static_frame in enumerate(static_frames):
+        output_frame_path = f'{output_path}/frame_{i + 1}.jpg'
+        cv2.imwrite(output_frame_path, static_frame)
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Время выполнения: {execution_time} секунд")
+
+    return frame_info
+
+
+def extract_images(file_name):
+    video_path = f'./video/${file_name}.mp4'
+    output_path = 'screen'
+    static_duration = 5
+    skip_frames = 20
+
+    return extract_static_frames(video_path, output_path, static_duration, skip_frames)
+
+
 app = FastAPI()
 origins = [
     "http://localhost.tiangolo.com",
@@ -128,22 +202,24 @@ def download_video(video_url, file_name, callback):
     end = time.time()
     print('[Download video] %ss' % round(end - start, 2))
 
+
 @app.get("/transcribe")
-def get_blog_post(video_url):
+def get_blog_post(video_url, background_tasks: BackgroundTasks):
     unique_id = str(uuid.uuid4())
 
-    result = None
-
     def transcribe_callback(a, b):
-        nonlocal result
         result = transcribe(unique_id)
+        images_info = extract_images(unique_id)
 
-    download_video(video_url, unique_id, transcribe_callback)
+    async def start_processing():
+        download_video(video_url, unique_id, transcribe_callback)
 
-    return result
+    background_tasks.add_task(start_processing)
+    return "Started processing..."
+
 
 @app.get("/video_info")
-def get_video_info(video_url):
+async def get_video_info(video_url):
     yt = YouTube(video_url)
 
     return {
